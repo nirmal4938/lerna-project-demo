16c9a1db112b39c70f264c76314dfe6082eda92c
nirmal kumar <nirmal.kumar@qpaix.com>
2024-07-06 08:58:47 +0530

    initial setup for web-ui

---
+++ packages/package-2/src/App.js
@@ -0,0 +1,12 @@
+import { Configure } from "package-1";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Package-2</h2>
+      <Configure />
+    </div>
+  );
+}
+
+export default App;
