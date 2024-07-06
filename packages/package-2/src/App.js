1f6250218469cc36bc2285bcc971dade85490511
nirmal kumar <nirmal.kumar@qpaix.com>
2024-07-06 09:05:47 +0530

    update 1.0

--- packages/package-2/src/App.js
+++ packages/package-2/src/App.js
@@ -3,6 +3,7 @@ import { Configure } from "package-1";
 function App() {
   return (
     <div className="App">
+      <h2>Welcome Lerna Learning -2</h2>
       <h2>Package-2</h2>
       <Configure />
     </div>
